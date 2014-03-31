763abd1ee8ad47ec83755bcdb45a7dd5a3d197f8
defiasto <defias.to@yandex.ru>
2014-03-31 23:11:12 +0300

    Picaso
    Simple Drawing App
    WTFPL v2

---
+++ Picaso/Picaso/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Picaso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawPanel = new System.Windows.Forms.Panel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.rectBtn = new System.Windows.Forms.Button();
+            this.elBtn = new System.Windows.Forms.Button();
+            this.penBtn = new System.Windows.Forms.Button();
+            this.lnBtn = new System.Windows.Forms.Button();
+            this.crvBtn = new System.Windows.Forms.Button();
+            this.transpBox = new System.Windows.Forms.CheckBox();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fIleToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(640, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fIleToolStripMenuItem
+            // 
+            this.fIleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fIleToolStripMenuItem.Text = "FIle";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // drawPanel
+            // 
+            this.drawPanel.BackColor = System.Drawing.Color.White;
+            this.drawPanel.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.drawPanel.Location = new System.Drawing.Point(0, 27);
+            this.drawPanel.Name = "drawPanel";
+            this.drawPanel.Size = new System.Drawing.Size(640, 480);
+            this.drawPanel.TabIndex = 1;
+            this.drawPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.drawPanel_Paint);
+            this.drawPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseClick);
+            this.drawPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseDown);
+            this.drawPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseMove);
+            this.drawPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseUp);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 550);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(640, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Bitmap|*.bmp";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Bitmap|*.bmp";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(442, 514);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 22);
+            this.button1.TabIndex = 8;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 517);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tool";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(498, 518);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Size";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(470, 514);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(22, 22);
+            this.button2.TabIndex = 10;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(531, 515);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(97, 20);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // rectBtn
+            // 
+            this.rectBtn.Location = new System.Drawing.Point(42, 512);
+            this.rectBtn.Name = "rectBtn";
+            this.rectBtn.Size = new System.Drawing.Size(39, 23);
+            this.rectBtn.TabIndex = 12;
+            this.rectBtn.Text = "Rect";
+            this.rectBtn.UseVisualStyleBackColor = true;
+            this.rectBtn.Click += new System.EventHandler(this.rectBtn_Click);
+            // 
+            // elBtn
+            // 
+            this.elBtn.Location = new System.Drawing.Point(87, 512);
+            this.elBtn.Name = "elBtn";
+            this.elBtn.Size = new System.Drawing.Size(47, 23);
+            this.elBtn.TabIndex = 13;
+            this.elBtn.Text = "Ellipse";
+            this.elBtn.UseVisualStyleBackColor = true;
+            this.elBtn.Click += new System.EventHandler(this.elBtn_Click);
+            // 
+            // penBtn
+            // 
+            this.penBtn.Location = new System.Drawing.Point(246, 512);
+            this.penBtn.Name = "penBtn";
+            this.penBtn.Size = new System.Drawing.Size(44, 23);
+            this.penBtn.TabIndex = 14;
+            this.penBtn.Text = "Pencil";
+            this.penBtn.UseVisualStyleBackColor = true;
+            this.penBtn.Click += new System.EventHandler(this.penBtn_Click);
+            // 
+            // lnBtn
+            // 
+            this.lnBtn.Location = new System.Drawing.Point(140, 512);
+            this.lnBtn.Name = "lnBtn";
+            this.lnBtn.Size = new System.Drawing.Size(47, 23);
+            this.lnBtn.TabIndex = 15;
+            this.lnBtn.Text = "Line";
+            this.lnBtn.UseVisualStyleBackColor = true;
+            this.lnBtn.Click += new System.EventHandler(this.lnBtn_Click);
+            // 
+            // crvBtn
+            // 
+            this.crvBtn.Location = new System.Drawing.Point(193, 512);
+            this.crvBtn.Name = "crvBtn";
+            this.crvBtn.Size = new System.Drawing.Size(47, 23);
+            this.crvBtn.TabIndex = 16;
+            this.crvBtn.Text = "Curve";
+            this.crvBtn.UseVisualStyleBackColor = true;
+            this.crvBtn.Click += new System.EventHandler(this.crvBtn_Click);
+            // 
+            // transpBox
+            // 
+            this.transpBox.AutoSize = true;
+            this.transpBox.Location = new System.Drawing.Point(296, 516);
+            this.transpBox.Name = "transpBox";
+            this.transpBox.Size = new System.Drawing.Size(130, 17);
+            this.transpBox.TabIndex = 17;
+            this.transpBox.Text = "Transparent (Rect, El)";
+            this.transpBox.UseVisualStyleBackColor = true;
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 572);
+            this.Controls.Add(this.transpBox);
+            this.Controls.Add(this.crvBtn);
+            this.Controls.Add(this.lnBtn);
+            this.Controls.Add(this.penBtn);
+            this.Controls.Add(this.elBtn);
+            this.Controls.Add(this.rectBtn);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.drawPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Picaso";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel drawPanel;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button rectBtn;
+        private System.Windows.Forms.Button elBtn;
+        private System.Windows.Forms.Button penBtn;
+        private System.Windows.Forms.Button lnBtn;
+        private System.Windows.Forms.Button crvBtn;
+        private System.Windows.Forms.CheckBox transpBox;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Picaso/Picaso/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picaso
+{
+    
+    public partial class Form1 : Form
+    {
+        private Bitmap GraphicsImage = new Bitmap(640, 480, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+        int size, mode;
+        Color fg, bg;
+        Point tP1, tP2;
+        Point[] bezi = new Point[4];
+        bool isDrawing = false;
+        int crvcnt = 0;
+        List<Point> points = new List<Point>();
+
+        private Rectangle mkRect(Point start, Point end) {
+            return new Rectangle(Math.Min(start.X, end.X), Math.Min(start.Y, end.Y), Math.Abs(start.X - end.X), Math.Abs(start.Y - end.Y));
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            Graphics.FromImage(GraphicsImage).Clear(Color.White);
+            mode = 2; //rect
+            size = 5;
+            numericUpDown1.Value = size;
+            fg = button1.BackColor;
+            bg = button2.BackColor;
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Graphics.FromImage(GraphicsImage).Clear(Color.White);
+            drawPanel.CreateGraphics().DrawImageUnscaled(GraphicsImage, new Point(0, 0));
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            GraphicsImage = new Bitmap(Image.FromFile(openFileDialog1.FileName), 640, 480);
+            //Graphics.FromImage(GraphicsImage).Dispose();
+            drawPanel.CreateGraphics().DrawImageUnscaled(GraphicsImage, new Point(0, 0));
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void drawPanel_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImageUnscaled(GraphicsImage, new Point(0, 0));
+        }
+
+        private void drawPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (mode == 2 || mode == 3 || mode == 0)
+            {
+                tP1 = e.Location;
+            }
+            else if (mode == 4)
+            {
+                points = new List<Point>();
+                points.Add(e.Location);
+                isDrawing = true;
+            }
+        }
+
+        private void drawPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+            if (mode != 1) tP2 = e.Location;
+            Graphics drgr = Graphics.FromImage(GraphicsImage);
+            if (mode == 2)
+            {
+                if (!transpBox.Checked)
+                    drgr.FillRectangle(new SolidBrush(bg), mkRect(tP1, tP2));
+                drgr.DrawRectangle(new Pen(fg, size), mkRect(tP1, tP2));
+                
+            }
+            else if (mode == 3)
+            {
+                if (!transpBox.Checked)
+                    drgr.FillEllipse(new SolidBrush(bg), mkRect(tP1, tP2));
+                drgr.DrawEllipse(new Pen(fg, size), mkRect(tP1, tP2));
+            }
+            else if (mode == 0)
+            {
+                drgr.DrawLine(new Pen(fg, size), tP1, tP2);
+            }
+            
+            
+            drawPanel.CreateGraphics().DrawImageUnscaled(GraphicsImage, new Point(0, 0));
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button1.BackColor = colorDialog1.Color;
+                fg = colorDialog1.Color;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button2.BackColor = colorDialog1.Color;
+                bg = colorDialog1.Color;
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            size = (int)numericUpDown1.Value;
+        }
+
+        private void drawPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                points.Add(e.Location);
+                Graphics drgr = Graphics.FromImage(GraphicsImage);
+                if (size > 1)
+                    drgr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                for (int i = 0; i < points.Count - 1; i++)
+                    drgr.DrawLine(new Pen(fg, size), points[i], points[i + 1]);
+                drawPanel.CreateGraphics().DrawImageUnscaled(GraphicsImage, new Point(0, 0));
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void rectBtn_Click(object sender, EventArgs e)
+        {
+            mode = 2; //rectangle
+            transpBox.Enabled = true;
+        }
+
+        private void elBtn_Click(object sender, EventArgs e)
+        {
+            mode = 3; //elipse
+            transpBox.Enabled = true;
+        }
+
+        private void lnBtn_Click(object sender, EventArgs e)
+        {
+            mode = 0; //line
+            transpBox.Enabled = false;
+        }
+
+        private void crvBtn_Click(object sender, EventArgs e)
+        {
+            mode = 1; //curve
+            transpBox.Enabled = false;
+        }
+
+        private void penBtn_Click(object sender, EventArgs e)
+        {
+            mode = 4; //pencil
+            transpBox.Enabled = false;
+        }
+
+        private void drawPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (mode == 1)
+            {
+                if (crvcnt < 4)
+                {
+                    bezi[crvcnt] = e.Location;
+                    crvcnt++;
+                }
+                if (crvcnt == 4)
+                {
+                    Graphics drgr = Graphics.FromImage(GraphicsImage);
+                    drgr.DrawBezier(new Pen(fg, size), bezi[0], bezi[1], bezi[2], bezi[3]);
+                    drawPanel.CreateGraphics().DrawImageUnscaled(GraphicsImage, new Point(0, 0));
+                    crvcnt = 0;
+                }
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            GraphicsImage.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GraphicsImage = new Bitmap(Image.FromFile("about.bmp"), 640, 480);
+            drawPanel.CreateGraphics().DrawImageUnscaled(GraphicsImage, new Point(0, 0));
+        }
+
+        
+    }
+}
